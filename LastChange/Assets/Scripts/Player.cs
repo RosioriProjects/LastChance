7ba01b5fc59e92f30e7964612323d17bc0db149e
bintescu <41958601+bintescu@users.noreply.github.com>
2020-05-06 14:08:53 +0300

    Miscare

--- LastChange/Assets/Scripts/Player.cs
+++ LastChange/Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Animator animator;
+    [SerializeField] float speed = 10.0f;
     void Start()
     {
+        animator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("isJump",true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            
+            animator.SetBool("isJump", false);
+        }
+        float transitionH = Input.GetAxis("Horizontal");
+        float transitionV = Input.GetAxis("Vertical");
+        animator.SetFloat("Horizontal", (Mathf.Abs(transitionH) + Mathf.Abs(transitionV)) * speed);
+         transitionH = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+         transitionV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Debug.Log(Input.GetAxis("Horizontal") * speed + " " + animator.GetFloat("Horizontal"));
+        transform.Translate(transitionH ,transitionV, 0.0f);
+       
     }
 }
