0c35cb4637589f4d8774d2a5c67ea64b103ea9f6
bintescu <41958601+bintescu@users.noreply.github.com>
2020-05-11 16:50:09 +0300

    Cateva Animatii

---
+++ LastChange/Assets/Scripts/Chest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private bool isOpen;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OpenChest()
+    {
+        animator.SetBool("Open", true);
+        Debug.Log("Am deschis Chest-ul");
+    }
+    public void CloseChest()
+    {
+        animator.SetBool("Open", false);
+        Debug.Log("Am inchis Chest-ul");
+    }
+}
--- LastChange/Assets/Scripts/Player.cs
+++ LastChange/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     Animator animator;
     [SerializeField] float speed = 1.0f;
     Rigidbody2D rb;
+    bool deschis = false;
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -23,7 +24,7 @@ public class Player : MonoBehaviour
         transitionH *= speed * Time.deltaTime;
         transitionV *= speed * Time.deltaTime;
         rb.velocity = new Vector2(transitionH, transitionV);
-        Debug.Log(transitionV);
+
         if (transitionV < 0f)
         {
            
@@ -40,5 +41,30 @@ public class Player : MonoBehaviour
 
         transform.Translate(transitionH ,transitionV, 0.0f);
        
+
+
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Chest")
+        {
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                if (deschis)
+                {
+                    FindObjectOfType<Chest>().CloseChest();
+                    deschis = false;
+                }
+                else
+                {
+                    FindObjectOfType<Chest>().OpenChest();
+                    deschis = true;
+                }
+            }
+        }
+    }
+
+
+
 }
