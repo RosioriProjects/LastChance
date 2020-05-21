8c4394f3d98e25315c56768eb39f349a63bf8566
Razvan2109 <visinica.razvan21@gmail.com>
2020-05-21 20:57:18 +0300

    MadeLoginScene

--- LastChange/Assets/Scripts/LoginData.cs
+++ LastChange/Assets/Scripts/LoginData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoginData : MonoBehaviour
@@ -13,49 +14,58 @@ public class LoginData : MonoBehaviour
     string user;
     string pass;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    IEnumerator Upload()
+    Dictionary<string, string> parameters = new Dictionary<string, string>();
+    public void doPost()
     {
         user = username.text;
         pass = password.text;
-        WWWForm form = new WWWForm();
-        form.AddField("username", user);
-        form.AddField("password", pass);
-        Debug.Log(user + pass);
+        string json = @"{
+        'username': 'usernamePlaceholder', 
+        'password':'passwordPlaceholder'
+        }";
+        string URL = "http://localhost:8080/user/login";
 
-        using (UnityWebRequest www = UnityWebRequest.Post("http://localhost:8080/user/login.json", form))
+        Dictionary<string, string> parameters = new Dictionary<string, string>();
+        //Auth token for http request
+        string accessToken;
+        //Our custom Headers
+        Dictionary<string, string> headers = new Dictionary<string, string>();
+        //Encode the access and secret keys
+        //Add the custom headers
+
+        parameters.Add("Content-Type", "application/json");
+        //parameters.Add("Content-Length", json.Length.ToString());
+        //Replace single ' for double " 
+        //This is usefull if we have a big json object, is more easy to replace in another editor the double quote by singles one
+        
+        json = json.Replace("usernamePlaceholder", user);
+        json = json.Replace("passwordPlaceholder", pass);
+        json = json.Replace("'", "\"");
+        //Encode the JSON string into a bytes
+        byte[] postData = System.Text.Encoding.UTF8.GetBytes(json);
+        //Now we call a new WWW request
+        WWW www = new WWW(URL, postData, parameters);
+        //And we start a new co routine in Unity and wait for the response.
+        StartCoroutine(WaitForRequest(www));
+    }
+    //Wait for the www Request
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        if (www.error == null)
         {
-            yield return www.SendWebRequest();
-
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                Debug.Log("Form upload complete!");
-            }
+            //Print server response
+            Debug.Log(www.text);
+            SceneManager.LoadScene("Game");
+        }
+        else
+        {
+            //Something goes wrong, print the error response
+            Debug.Log(www.error);
         }
     }
-    public void Login()
-    {
 
-        
-        //StartCoroutine(Upload());
-        Debug.Log(StartCoroutine(Upload()));
-        
-
-    }
+   
 
 
 }
